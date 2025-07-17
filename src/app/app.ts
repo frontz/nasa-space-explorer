3b76f9391c4df17ff8e152dbaa56137ae6a28bf3
Tomasz <tom.frontz@gmail.com>
2025-07-17 10:37:43 +0200

    Basic app skeleton

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Apod} from './features/apod/apod';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Apod, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
