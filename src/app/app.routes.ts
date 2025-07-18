f2498b0847f12a815a06a7b22e4fc4872a5e9bf8
Tomasz <tom.frontz@gmail.com>
2025-07-18 11:19:57 +0200

    Sidebar with menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Apod } from './features/apod/apod';
+import { Gallery } from './features/gallery/gallery';
+import { Home } from './features/home/home';
+import { Contact } from './features/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home},
+  { path: 'apod', component: Apod},
+  { path: 'gallery', component: Gallery},
+  { path: 'contact', component: Contact },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', redirectTo: '/apod' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Apod} from './features/apod/apod';
-import { MatToolbar } from '@angular/material/toolbar';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import { Apod } from './features/apod/apod';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Apod, MatToolbar],
+  imports: [RouterOutlet, Apod, MatSidenavModule, MatMenu, MatMenuTrigger, MatButton, MatMenuItem, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
