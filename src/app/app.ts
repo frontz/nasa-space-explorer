6504a19f4f3e514e0a12798dffa2f67f999b3e8c
Tomasz <tom.frontz@gmail.com>
2025-08-06 14:37:02 +0200

    Background with stars animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import { ApodComponent } from './features/apod/apod.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatDrawer, MatDrawerContainer, MatDrawerContent} from '@angular/material/sidenav';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import {SpaceBackgroundComponent} from './shared/background-component/background-component';
 
-import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [ApodComponent, RouterOutlet, MatSidenavModule, MatButton,  RouterLink],
+  standalone: true,
+  imports: [
+    MatDrawerContainer,
+    MatDrawer,
+    RouterOutlet,
+    MatButton,
+    RouterLink,
+    SpaceBackgroundComponent,
+    MatDrawerContent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'nasa-space-explorer';
 }
