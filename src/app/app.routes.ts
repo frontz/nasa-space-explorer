bb9697f82a77fe8a52e2666a62ec235162726c52
Tomasz <tom.frontz@gmail.com>
2025-09-02 11:48:43 +0200

    Home component merged with Apod

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { ApodComponent } from './features/apod/apod.component';
 import { Gallery } from './features/gallery/gallery';
 import { Home } from './features/home/home';
 import { Contact } from './features/contact/contact';
@@ -7,7 +6,6 @@ import {DetailsComponent} from './features/details-component/details-component';
 
 export const routes: Routes = [
   { path: 'home', component: Home},
-  { path: 'apod', component: ApodComponent},
   { path: 'gallery', component: Gallery},
   { path: 'details/:id', component: DetailsComponent },
   { path: 'contact', component: Contact },
